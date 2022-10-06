663ce92d1e4367fd7facb08daf827b87cb53629c
Adam Gąsowski <adam.gasowski@gander.pl>
2022-10-06 10:22:42 +0200

    config usage of VueUse and Pinia. Tree shaking: not used => not required.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import monkey, {cdn} from 'vite-plugin-monkey';
+import monkey, {cdn, util} from 'vite-plugin-monkey';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,8 +20,25 @@ export default defineConfig({
             build: {
                 fileName: 'redmine-dev-sum.user.js',
                 externalGlobals: {
-                    vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
-                    lodash: cdn.jsdelivr('_', 'lodash.min.js'),
+                    'vue': cdn
+                        .jsdelivr('Vue', 'dist/vue.global.prod.js')
+                        .concat(
+                            await util.fn2dataUrl(() => {
+                                // @ts-ignore
+                                window.Vue = Vue;
+                            }),
+                        ),
+                    '@vueuse/core': [
+                        'VueUse',
+                        (version: string) => `https://cdn.jsdelivr.net/npm/@vueuse/shared@${version}/index.iife.min.js`,
+                        (version: string) => `https://cdn.jsdelivr.net/npm/@vueuse/core@${version}/index.iife.min.js`,
+                        await util.fn2dataUrl(() => {
+                            // @ts-ignore
+                            window.VueUse = VueUse;
+                        }),
+                    ],
+                    'pinia': cdn.jsdelivr('Pinia', 'dist/pinia.iife.prod.js'),
+                    'lodash': cdn.jsdelivr('_', 'lodash.min.js'),
                 },
             },
         }),
